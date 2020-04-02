e7c2d7d66c43e57e18a705a6a9a7e06f81d480dd
Karthick31 <karthickkaruna31@gmail.com>
2020-04-02 10:26:44 +0530

    Update user_add.sh

--- user_add.sh
+++ user_add.sh
@@ -3,14 +3,12 @@
 # This script creates an account on the system.
 
 USERNAME=$1
-USERID=$2
-COMMENT=$3
-HOMEDIR=$4
+
 
 
 # Create the user.
-/usr/sbin/useradd -m -u $USERID -c "$COMMENT" -g $USERID -d $HOMEDIR -s /bin/bash $USERNAME
+/usr/sbin/useradd -m -s /bin/bash $USERNAME
 
 # Set the password for the user.
-echo ${USERNAME} | passwd --stdin ${USER_NAME}
+echo ${USERNAME} | passwd --stdin ${USERNAME}
 
