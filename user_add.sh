bd7cc97c5d79873eb691561c5ebfa27f4197c884
Karthick31 <karthickkaruna31@gmail.com>
2020-04-02 04:28:55 +0000

    first commit

---
+++ user_add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script creates an account on the system.
+
+USER_NAME=$1
+
+
+# Create the user.
+useradd  -m ${USER_NAME}
+
+# Set the password for the user.
+echo ${USER_NAME} | passwd --stdin ${USER_NAME}
+
