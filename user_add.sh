b0ba91153d527a5563dbb4b2c3e82cc3878fbf51
Karthick31 <karthickkaruna31@gmail.com>
2020-04-09 21:10:47 +0530

    Update user_add.sh

--- user_add.sh
+++ user_add.sh
@@ -10,5 +10,5 @@ USERNAME=$1
 /usr/sbin/useradd -m -s /bin/bash $USERNAME
 
 # Set the password for the user.
-echo '${USERNAME}:${USERNAME}' | sudo chpasswd
+echo '${USERNAME}:${USERNAME}' | chpasswd
 
